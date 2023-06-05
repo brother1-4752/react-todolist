8cfcb23e06371f8b74abd6a2898eda171e4d92b7
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-06-06 01:44:24 +0900

    feat : 손글씨 폰트 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import Handwriting from "../fonts/Handwriting.ttf";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: "Handwriting";
+        src : local('Handwriting'), local('Handwriting');
+        font-style: normal;
+        src: url(${Handwriting}) format('truetype')
+    }
     body {
         background-color: ${(props) => props.theme.bgColor};
         color: ${(props) => props.theme.textColor};
-        
+        font-family: Handwriting;
     }
 `;
 
