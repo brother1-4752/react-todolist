0b89d81748fa50e75004f0b3cf48c8b293c4829a
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-06-06 02:33:06 +0900

    deploy : router 재설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,7 @@ import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
 
 import "./styles/reset.css";
-<<<<<<< HEAD
 import router from "./router.tsx";
-=======
-import router from "./router";
->>>>>>> c44b17cfcf4f1dae6542c732e59d7ccfcaa9d29e
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
